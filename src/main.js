88de3cb8e530b082c8195496dd385c0353a185d4
Simeon Rediger <simeonrediger@gmail.com>
2025-12-25 01:22:45 -0500

    Add test key-value pairs

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
 import HashMap from './hash-map.js';
 
 const map = new HashMap();
+
+map.set('apple', 'red');
+map.set('banana', 'yellow');
+map.set('carrot', 'orange');
+map.set('dog', 'brown');
+map.set('elephant', 'gray');
+map.set('frog', 'green');
+map.set('grape', 'purple');
+map.set('hat', 'black');
+map.set('ice cream', 'white');
+map.set('jacket', 'blue');
+map.set('kite', 'pink');
+map.set('lion', 'golden');
