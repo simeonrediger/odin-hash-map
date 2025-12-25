631984bd6c2cfdd5175bdc171e8a0bdb67e251ef
Simeon Rediger <simeonrediger@gmail.com>
2025-12-24 22:33:53 -0500

    Add hashing function

--- src/hash-map.js
+++ src/hash-map.js
@@ -1,4 +1,20 @@
 export default class HashMap {
     #loadFactor = 0.75;
     #capacity = 16;
+
+    #hash(key) {
+        if (typeof key !== 'string') {
+            throw new TypeError(
+                `HashMap keys must be strings. Got ${typeof key}.`,
+            );
+        }
+
+        let hash = 0;
+
+        for (let i = 0; i < key.length; i++) {
+            hash = (hash * 31 + key.charCodeAt(i)) % this.#capacity;
+        }
+
+        return hash;
+    }
 }
