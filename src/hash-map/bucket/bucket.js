5c152dc22922839d6d0f2532c451823b9a40aba9
Simeon Rediger <simeonrediger@gmail.com>
2025-12-25 03:48:13 -0500

    Remove unused methods

--- src/hash-map/bucket/bucket.js
+++ src/hash-map/bucket/bucket.js
@@ -9,14 +9,6 @@ export default class Bucket {
         return this.#size;
     }
 
-    get head() {
-        return this.#head;
-    }
-
-    get tail() {
-        return this.#tail;
-    }
-
     nodes() {
         const nodes = [];
         let node = this.#head;
@@ -33,18 +25,6 @@ export default class Bucket {
         if (this.#head) {
             this.#tail.next = new Node(key, value);
             this.#tail = this.#tail.next;
-        } else {
-            this.#head = new Node(key, value);
-            this.#tail = this.#head;
-        }
-
-        this.#size++;
-    }
-
-    prepend(key, value) {
-        if (this.#head) {
-            const head = this.#head;
-            this.#head = new Node(key, value, head);
         } else {
             this.#head = new Node(key, value);
             this.#tail = this.#head;
@@ -132,49 +112,6 @@ export default class Bucket {
         return null;
     }
 
-    findIndex(key) {
-        let node = this.#head;
-
-        for (let i = 0; node; i++) {
-            if (node.key === key) {
-                return i;
-            }
-
-            node = node.next;
-        }
-
-        return -1;
-    }
-
-    insertAt(index, ...entries) {
-        if (index === 0) {
-            for (let i = entries.length - 1; i >= 0; i--) {
-                this.prepend(...entries[i]);
-            }
-
-            return;
-        } else if (index === this.#size) {
-            for (const entry of entries) {
-                this.append(...entry);
-            }
-
-            return;
-        } else if (index < 0 || index > this.#size) {
-            throw new RangeError(
-                `Insertion index must be within [0, ${this.#size}]`,
-            );
-        }
-
-        let prevNode = this.at(index - 1);
-
-        for (const entry of entries) {
-            const nextNode = prevNode.next;
-            prevNode.next = new Node(...entry);
-            prevNode = prevNode.next;
-            this.#size++;
-        }
-    }
-
     remove(key) {
         const node = this.find(key);
 
@@ -199,39 +136,5 @@ export default class Bucket {
 
             currentNode = currentNode.next;
         }
-    }
-
-    removeAt(index) {
-        if (index === 0) {
-            this.shift();
-            return;
-        } else if (index === this.#size - 1) {
-            this.pop();
-            return;
-        } else if (index < 0 || index >= this.#size) {
-            throw new RangeError(
-                `Removal index must be within [0, ${this.#size})`,
-            );
-        }
-
-        const prevNode = this.at(index - 1);
-        prevNode.next = prevNode.next.next;
-        this.#size--;
-    }
-
-    toString() {
-        let node = this.#head;
-        let string = '';
-
-        while (node) {
-            string += `( ${node.key}: ${node.value} ) -> `;
-            node = node.next;
-        }
-
-        if (node === null) {
-            string += node;
-        }
-
-        return string;
     }
 }
